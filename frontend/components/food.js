f998ba49a99d3c2318b56907f4aad54eb3f56272
garedan <danyG22@proton.me>
2023-05-06 16:16:45 -0300

    Leyendo desde el contrato

--- frontend/components/food.js
+++ frontend/components/food.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import Link from 'next/link'
 import { convertToPath } from "../lib/items";
+import Image from 'next/image'
 
 export default function Food ({ item, showAs }) {
     //console.log(item);
+     /* uint32 id;
+  address creator_address;
+  string topic; */
     if (showAs === "Page") {
         return (
            
-            <div key={item.pais} className="border shadow rounded-xl overflow-hidden" >
+            <div key={item.id} className="border shadow rounded-xl overflow-hidden" >
                 <img style={{ height: "20rem", margin: 'auto' }} src={item.image} />
                 <div className="p-4" >
                     <span
                     style={{ height: "64px", margin: '45%' }}
                     className="text-2xl font-semibold"
                     >
-                    {item.title}
+                    {item.creator_address}
                     </span>
                     <div style={{ height: "70px", overflow: "hidden" }}>
                     {/* <p>{dishes.name}</p> */}
-                    <p className="text-gray-400">{item.pais}</p>
+                    <p className="text-gray-400">{item.topic}</p>
                     </div>
                 </div>
             </div>
@@ -49,20 +53,36 @@ export default function Food ({ item, showAs }) {
      return (
         <div>
           <div>
-            <Link href={`/posts/${convertToPath(item.title)}`}>
+            <Link href={`/posts/${item.creator_address}`}>
               <a>
-              <div key={item.pais} className="border shadow rounded-xl overflow-hidden">
-                <img style={{ height: "20rem" }} src={item.image} />
+              <div key={item.id} className="border shadow rounded-xl overflow-hidden">
+                {(item.topic === 'Javascript' || item.topic === '') &&
+                    <Image
+                    src="/JavaScript-logo.png"
+                    fill
+                    width={200}
+                    height={200}
+                  />}
+                    {/* <img style={{ height: "20rem" }} src={item.image} /> */}
+                {
+                    (item.topic === 'React') &&
+                    <Image
+                    src="/react.jpg"
+                    fill
+                    width={200}
+                    height={200}
+                  />
+                }
                 <div className="p-4">
                     <p
                     style={{ height: "64px" }}
-                    className="text-2xl font-semibold"
+                    className="font-semibold"
                     >
-                    {item.title}
+                    {item.creator_address}
                     </p>
                     <div style={{ height: "70px", overflow: "hidden" }}>
                     {/* <p>{dishes.name}</p> */}
-                    <p className="text-gray-400">{item.pais}</p>
+                    <p className="text-gray-400">{item.topic}</p>
                     </div>
                 </div>
             </div>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -36,13 +36,13 @@ export default function Home({ items }) {
     getAllArticles();
   }, []);
  
-
+ 
   return (
     <div className="flex justify-center">
       <div className="px-4" style={{ maxWidth: "1600px" }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {items &&
-          items.map((item, i) => (
+          {articles &&
+          articles.map((item, i) => (
             //<Product key={item.id} item={item} showAs="item" />
             <Food key={item.id} item={item} showAs="item" />
             
